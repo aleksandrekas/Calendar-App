39aadb4cf8e1884cc3774e6db6e4b67985bc8dd7
aleksandrekas <154304980+aleksandrekas@users.noreply.github.com>
2024-12-08 16:42:23 +0400

    starting steps of project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+
+
+
+
+function App() {
+  return(
+    <div id="container">
+      <div id="daysHeader">
+        <div>Monday</div>
+        <div>Tuesday</div>
+        <div>Wednesday</div>
+        <div>Thursday</div>
+        <div>Friday</div>
+        <div>Saturday</div>
+        <div>Sunday</div>
+      </div>
+    </div>
+  )
+
+
+}
+
+export default App
