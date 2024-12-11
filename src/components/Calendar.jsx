ca67a08eddaaec553d22b86ec1066a0c0ea658a7
aleksandrekas <154304980+aleksandrekas@users.noreply.github.com>
2024-12-11 22:48:02 +0400

    fixed current day issue

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -4,12 +4,11 @@ import { useSelector } from "react-redux"
 export default function Calendar(){
     const monthDaysState = useSelector((state) => state.monthDays);
     const today = new Date();
-    const currentDay = today.toLocaleDateString('ka-GE', { day: 'numeric' });
+    const currentDay = parseInt(today.toLocaleDateString('ka-GE', { day: 'numeric' }));
     const currentMonth = today.toLocaleDateString('ka-GE', { month: 'numeric' }) - 1; 
-    const currentYear = today.toLocaleDateString('ka-GE', { year: 'numeric' });
+    const currentYear = parseInt(today.toLocaleDateString('ka-GE', { year: 'numeric' }));
 
-
-    console.log(currentDay,currentMonth,currentYear)
+ 
     return (
         <div id="calendarBody">
             {Array.from({length:monthDaysState.paddingDays}).map((_,index)=>(
@@ -18,10 +17,9 @@ export default function Calendar(){
             {Array.from({length:monthDaysState.totalDays}).map((_,index)=>{
                 const dayNum = index + 1; 
                 const isToday =
-                    dayNum === currentDay &&
-                    monthDaysState.month === currentMonth &&
-                    monthDaysState.year === currentYear;
-
+                    dayNum === currentDay&&
+                    monthDaysState.date.getMonth() === currentMonth &&
+                    monthDaysState.date.getFullYear() === currentYear;
                 return(
                     <Day key={index} isCurrDay={isToday} typeClass={''} dayNum={dayNum} />
                 )
